4e067544ffe26b4a0fe3e060952ef4f30cd949e6
윤득렬 <ryeoly@bluemango.me>
2020-11-19 19:42:07 +0900

    main page(#2)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,6 @@
 <template>
-  <div class="wrapper">
-
-    <div class="one" style="text-align: right; color:white" >2016722021 윤인석</div>
-    <div class="two" style="background-color: midnightblue">Two</div>
-    <div class="three" style="background-color:white">Three</div>
-    <div class="four" style="background-color:white">
-      <img src="./Bluemango_logo.jpeg" alt="Logo" title="bluemango">
-      <ul class="mylist">
-        <li><a href="url">기능1</a></li>
-        <li><a href="url">기능2</a></li>
-        <li><a href="url">기능3</a></li>
-        <li><a href="url">기능4</a></li>
-      </ul>
-      <hr color="gray" size="3px" width="80%">
-      <ul class="friend">
-        <li><a href="url2">친구1</a></li>
-        <li><a href="url2">친구2</a></li>
-        <li><a href="url2">친구3</a></li>
-        <li><a href="url2">친구4</a></li>
-      </ul>
-    </div>
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
@@ -28,58 +9,3 @@
     name: 'App'
   }
 </script>
-
-<style>
-  ul{
-    list-style: none;
-    line-height: 3em;
-  }
-  div{
-    background-color: aliceblue;
-  }
-  img{
-    max-width: 100%;
-    height: auto !important;
-    line-height: 0px;
-  }
-  .wrapper {
-    display: grid;
-    grid-template-columns: repeat(77, 1fr);
-    grid-template-rows: repeat(100, 1fr);
-    height: 100vh;
-  }
-  .one {
-    grid-column: 12/78;
-    grid-row: 1/28;
-    border-radius: 5px 5px 5px 5px;
-    background : linear-gradient(87deg, #5e72e4 0, #825ee4 100%);
-    padding-top: 10px;
-    padding-right: 5px;
-    background-color: #5758bb;
-    box-shadow: 2px 4px 2px lightgray;
-    font-weight: 500
-  ;
-  }
-  .two {
-    grid-column: 15 / 45;
-    grid-row: 23 / 50;
-    border-radius: 5px 5px 5px 5px;
-    background-color: #5758bb;
-    box-shadow: 0px 2px 0px lightgray;
-  }
-  .three {
-    grid-column: 47/75;
-    grid-row: 23 / 50;
-    border-radius: 5px 5px 5px 5px;
-    background-color: #5758bb;
-    box-shadow: 2px 2px 2px lightgray;
-  }
-  .four {
-    grid-column: 1/11;
-    grid-row: 1/100;
-    background-color: #5758bb;
-    box-shadow: 2px 2px 2px lightgray;
-  }
-
-
-</style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,113 +1,118 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="wrapper">
+    <div class="one" style="text-align: right; color:white" >2016722021 윤인석</div>
+    <div id="transition-components-demo">
+      <input type="radio" value="v-a" id="a" name="view" v-model="view">
+      <label for="a">A</label>
+      <input type="radio" value="v-b" id="b" name="view" v-model="view">
+      <label for="b">B</label>
+      <transition name="component-fade" mode="out-in">
+        <component v-bind:is="view"></component>
+      </transition>
+    </div>
+
+
+    <div class="two" style="background-color: midnightblue">Two</div>
+    <div class="three" style="background-color:white">Three</div>
+    <div class="four" style="background-color:white">
+      <img src="../assets/Bluemango_logo.jpeg" alt="Logo" title="bluemango">
+      <ul class="mylist">
+        <li><a href="url">main</a></li>
+        <li><a href="url">grade</a></li>
+        <li><a href="url">기능3</a></li>
+        <li><a href="url">기능4</a></li>
+      </ul>
+      <hr color="gray" size="3px" width="80%">
+      <ul class="friend">
+        <li><a href="url2">친구1</a></li>
+        <li><a href="url2">친구2</a></li>
+        <li><a href="url2">친구3</a></li>
+        <li><a href="url2">친구4</a></li>
+      </ul>
+    </div>
+
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  el: '#transition-components-demo',
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      view: ''
     }
+  },
+  components: {
+      'v-a': {
+        template: '<div>Component A</div>'
+      },
+      'v-b': {
+        template: '<div>Component B</div>'
+      }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style>
+  .component-fade-enter-active, .component-fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  .component-fade-enter, .component-fade-leave-to {
+    opacity: 0;
+  }
+
+
+  ul{
+    list-style: none;
+    line-height: 3em;
+  }
+  div{
+    background-color: aliceblue;
+  }
+  img{
+    max-width: 100%;
+    height: auto !important;
+    line-height: 0px;
+  }
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(77, 1fr);
+    grid-template-rows: repeat(100, 1fr);
+    height: 100vh;
+  }
+  .one {
+    grid-column: 12/78;
+    grid-row: 1/28;
+    border-radius: 5px 5px 5px 5px;
+    background : linear-gradient(87deg, #5e72e4 0, #825ee4 100%);
+    padding-top: 10px;
+    padding-right: 5px;
+    background-color: #5758bb;
+    box-shadow: 2px 4px 2px lightgray;
+    font-weight: 500
+  ;
+  }
+  .two {
+    grid-column: 15 / 45;
+    grid-row: 23 / 50;
+    border-radius: 5px 5px 5px 5px;
+    background-color: #5758bb;
+    box-shadow: 0px 2px 0px lightgray;
+  }
+  .three {
+    grid-column: 47/75;
+    grid-row: 23 / 50;
+    border-radius: 5px 5px 5px 5px;
+    background-color: #5758bb;
+    box-shadow: 2px 2px 2px lightgray;
+  }
+  .four {
+    grid-column: 1/11;
+    grid-row: 1/100;
+    background-color: #5758bb;
+    box-shadow: 2px 2px 2px lightgray;
+  }
 </style>
